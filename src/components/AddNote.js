00b5a69f9f53446640f25dc52af62f9657622b71
805ale <aleeevaida@gmail.com>
2023-01-30 08:50:08 +0200

    buttons

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,15 +1,23 @@
 import styles from "/styles/Home.module.css";
+import { TextField } from "@material-ui/core";
+import { Button } from "@mui/material";
 
 const AddNote = () => {
     return(<div className={styles.add}>
-        <textarea className={styles.textarea}
-        rows="8" 
-        cols="10"
+        <TextField
+        variant="standard"
+        rows={8} 
+        cols={10}
         placeholder="Type to add a note..."
-        ></textarea>
+        />
         <div className={styles.footer}>
             <small>200 Remaining</small>
-            <button className={styles.save}>Save</button>
+            <Button 
+            variant="text"
+            onClick={() => {
+                alert('clicked');
+              }}
+            >Save</Button>
         </div>
     </div>)
 }
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,12 +1,15 @@
-import { MdDeleteForever } from 'react-icons/md';
+import * as React from 'react';
 import styles from "/styles/Home.module.css";
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const Note = ({ id, text, date }) => {
     return <div className={styles.notes}>
             <span>{text}</span>
                 <div className={styles.footer}>
                     <small>28/01/2023</small>
-                    <MdDeleteForever className='delete-icon' size='1.3em' />
+                    <IconButton aria-label="delete"><DeleteIcon />
+                    </IconButton>
                 </div>
         </div>
 };
